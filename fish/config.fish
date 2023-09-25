c4ee37253ac3a44977691cb6daca94c2ae124ff7
Artur Dernst <artur.dernst@gmail.com>
2023-09-25 22:51:57 +0200

    more subdirs

--- fish/config.fish
+++ fish/config.fish
@@ -2,3 +2,11 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set fish_greeting
 end
+
+fish_add_path /home/artur/.local/anaconda3/bin/
+
+alias tm="tmux"
+alias tma="tmux attach"
+alias nv="nvim"
+alias pd="prevd"
+alias nd="nextd"
