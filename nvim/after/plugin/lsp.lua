0635d74625a95cd8843b84e3f759527cc40d95b9
Artur Dernst <artur.dernst@gmail.com>
2023-09-25 23:33:26 +0200

    nvim remapped function declaration helper to ctrl-i because of tmux vim navigator

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -38,7 +38,7 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "<leader>ep", function() vim.diagnostic.goto_prev() end, opts)
   vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
   vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+  vim.keymap.set("i", "<C-i>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 lsp.setup()
