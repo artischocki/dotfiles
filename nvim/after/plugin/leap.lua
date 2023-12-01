4071d8f1d26dae6bfc370880555b395b6d744ed4
Artur Dernst <artur.dernst@gmail.com>
2023-12-01 13:42:31 +0100

    changed leap keybinding to mn

--- nvim/after/plugin/leap.lua
+++ nvim/after/plugin/leap.lua
@@ -1,5 +1,5 @@
 -- i only want bidirectional search
-vim.keymap.set("n", "<leader>f", function ()
+vim.keymap.set("n", "mn", function ()
   local current_window = vim.fn.win_getid()
   require('leap').leap { target_windows = { current_window } }
 end)
